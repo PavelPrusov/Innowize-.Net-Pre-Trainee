7c3bca4b628e9b22162e7c9f6dc8152ed8becf78
PavelPrusov <pavelprusovcorpwork@mail.ru>
2025-10-01 19:57:57 +0300

    feat: basic console calculator with some validation

---
+++ 1 - Calculator/Calculator.cs
@@ -0,0 +1,11 @@
+﻿namespace _1___Calculator
+{
+    public class Calculator
+    {
+        public double Sum(double x, double y) => x + y;
+        public double Sub(double x, double y) => x - y;
+        public double Mult(double x, double y) => x * y;
+        public double Div(double x, double y) => y != 0 ? x / y : throw new DivideByZeroException();
+
+    }
+}
--- 1 - Calculator/Program.cs
+++ 1 - Calculator/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using _1___Calculator;
+
+Calculator calculator = new Calculator();
+
+while (true)
+{
+    double firstNumber = 0;
+    double secondNumber = 0;
+
+    int operationNumber = 0;
+
+    double result = 0;
+
+    Console.WriteLine("Введите 1-ое число: ");
+
+    while(!double.TryParse(Console.ReadLine(), out firstNumber))
+    {
+        Console.WriteLine("Неправильный формат данных");
+    }
+
+    Console.WriteLine("Выберите операцию:");
+    Console.WriteLine("1.+ (сложение)");
+    Console.WriteLine("2.- (вычитание)");
+    Console.WriteLine("3.* (умножение)");
+    Console.WriteLine("4./ (деление)");
+    Console.WriteLine();
+
+    while (!int.TryParse(Console.ReadLine(), out operationNumber)||(operationNumber>4||operationNumber<1))
+    {
+        Console.WriteLine("неверная операция");
+    }
+
+    Console.WriteLine("Введите 2-ое число: ");
+
+    while(!double.TryParse(Console.ReadLine(), out secondNumber))
+    {
+        Console.WriteLine("Неправильный формат данных");
+    }
+
+    try
+    {
+        switch (operationNumber)
+        {
+            case 1: result = calculator.Sum(firstNumber, secondNumber); break;
+            case 2: result = calculator.Sub(firstNumber, secondNumber); break;
+            case 3: result = calculator.Mult(firstNumber, secondNumber); break;
+            case 4: result = calculator.Div(firstNumber, secondNumber); break;
+            default: throw new InvalidOperationException("неизвестная операция");
+        }
+
+        Console.WriteLine($"Результат: {result}\n");
+    }
+    catch(Exception e)
+    {
+        Console.WriteLine($"Ошибка: {e.Message}");
+    }
+   
+    Console.WriteLine("Хотите продолжить? (y)");
+    if (Console.ReadLine()?.ToLower() != "y")
+        break;
+}
