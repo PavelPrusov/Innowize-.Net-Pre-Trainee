445fa2b5ce7fa129a187bf021b29b1519b331226
PavelPrusov <pavelprusovcorpwork@mail.ru>
2025-10-10 23:12:58 +0300

    fix: now N-layer architecture

--- Library.Domain/Entities/Book.cs
+++ Library.Domain/Entities/Book.cs
@@ -3,7 +3,7 @@
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }= string.Empty;
+        public string Title { get; set; } = string.Empty;
         public int PublishedYear { get; set; }
         public int AuthorId { get; set; }
     }
