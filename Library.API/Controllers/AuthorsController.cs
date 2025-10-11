b1deea3c2a8a64ebe99597181c0d6202d8f2df6b
PavelPrusov <pavelprusovcorpwork@mail.ru>
2025-10-11 13:51:35 +0300

    feat(API): controllers realization

--- Library.API/Controllers/AuthorsController.cs
+++ Library.API/Controllers/AuthorsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Library.BusinessLogic.DTO.Author;
+using Library.BusinessLogic.Interfaces;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers
@@ -7,5 +9,49 @@ namespace Library.API.Controllers
     [ApiController]
     public class AuthorsController : ControllerBase
     {
+        private readonly IAuthorService _authorService;
+
+        public AuthorsController(IAuthorService authorService)
+        {
+            _authorService = authorService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<AuthorDto>>> GetAll()
+        {
+            var authors = await _authorService.GetAllAsync();
+            return Ok(authors);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AuthorDto>> GetById(int id)
+        {
+            var author = await _authorService.GetByIdAsync(id);
+            if (author == null) return NotFound();
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AuthorDto>> Add(CreateAuthorDto authorDto)
+        {
+            var author = await _authorService.CreateAsync(authorDto);
+            return CreatedAtAction(nameof(GetById), new { id = author.Id }, author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<AuthorDto>> Update(int id, UpdateAuthorDto authorDto)
+        {
+            var author = await _authorService.UpdateAsync(id, authorDto);
+            if (author == null) return NotFound();
+            return Ok(author);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var result = await _authorService.DeleteAsync(id);
+            if (!result) return NotFound();
+            return NoContent();
+        }
     }
 }
--- Library.API/Controllers/BooksController.cs
+++ Library.API/Controllers/BooksController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Library.BusinessLogic.DTO.Book;
+using Library.BusinessLogic.Interfaces;
+using Library.Domain.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Library.API.Controllers
@@ -7,5 +10,48 @@ namespace Library.API.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly IBookService _bookService;
+
+        public BooksController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        [HttpGet]
+        public  async Task<ActionResult<List<BookDto>>> GetAll()
+        {
+            var books = await _bookService.GetAllAsync();
+            return Ok(books);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<BookDto>> GetById(int id)
+        {
+            var book = await _bookService.GetByIdAsync(id);
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<BookDto>> Add(CreateBookDto bookDto)
+        {
+            var newBook = await _bookService.CreateAsync(bookDto);
+            return CreatedAtAction(nameof(GetById), new { id = newBook.Id }, newBook);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<BookDto>> Update(int id,UpdateBookDto bookDto)
+        {
+            var updatedBook = await _bookService.UpdateAsync(id,bookDto);
+            return Ok(updatedBook);
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var deletedBook = await _bookService.DeleteAsync(id);
+            if (!deletedBook) return NotFound();
+            return NoContent();
+        }
+
+
     }
 }
